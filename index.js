3ca4f88022afdf9ce3ad76c9b199832473f123de
hmelo1 <heriberto@melo.nyc>
2017-10-07 18:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -42,9 +42,9 @@ function placeOrder(cardNumber) {
 var cart2 = {lemons: 64, mangos: 95, bananas: 32}
 var itemsInCart = []
 for (let i = 0; i < Object.keys(cart2).length; i++){
-itemsInCart.push(cart2)
+  itemsInCart.push(cart2)
 //for (var prop in cart2){
-  
+
   /*if (Object.keys(cart2).length == 1){
     console.log(`In your cart, you have ${prop} at ${cart2[prop]}`)
   }
