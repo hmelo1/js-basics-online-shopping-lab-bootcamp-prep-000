eb5039fc59e33c648c43c9beb8f1ad191fc44e45
hmelo1 <heriberto@melo.nyc>
2017-10-07 18:42:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -29,10 +29,10 @@ function viewCart() {
   if (Object.keys(cart).length === 1){
     console.log(`In your cart, you have ${itemsInCart[0]}.`)
   }
-  else if (Object.keys(cart).length === 2){
+  if (Object.keys(cart).length === 2){
     console.log(`In your cart, you have ${itemsInCart[0]} and ${itemsInCart[1]}.`)
   }
-  else if (Object.keys(cart).length >= 3){
+  if (Object.keys(cart).length >= 3){
     console.log(`In your cart, you have ${itemsInCart[0]}, ${itemsInCart[1]}, and ${itemsInCart[2]}.`)
   }
 }
