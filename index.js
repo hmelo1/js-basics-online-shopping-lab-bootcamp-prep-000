0b3d2a583863650ae0078ec7ffa000391464a4e7
hmelo1 <heriberto@melo.nyc>
2017-10-07 18:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -48,10 +48,10 @@ for (var prop in cart2){
   if (Object.keys(cart2).length === 1){
     console.log(`In your cart, you have ${itemsInCart[0]}.`)
   }
-  if (Object.keys(cart2).length === 2){
+  elseif (Object.keys(cart2).length === 2){
     console.log(`In your cart, you have ${itemsInCart[0]} and ${itemsInCart[1]}.`)
   }
-  if (Object.keys(cart2).length >= 3){
+  elseif (Object.keys(cart2).length >= 3){
     console.log(`In your cart, you have ${itemsInCart[0]}, ${itemsInCart[1]}, and ${itemsInCart[2]}`)
   }
   /*if (Object.keys(cart2).length == 1){
