a523d8777c12c4ab093448f8d44444f41068f774
hmelo1 <heriberto@melo.nyc>
2017-10-07 19:54:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,10 +19,7 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
-  var itemsInCart = []
-  for (var prop in cart){
-    itemsInCart.push(`${prop} at $${cart[prop]}`)
-  }
+  
   if (!cart.length){
     console.log("Your shopping cart is empty.")
   }
