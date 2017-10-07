fd13dc6171dcd0a82db2328dd92d70f84b54d44e
hmelo1 <heriberto@melo.nyc>
2017-10-07 19:42:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ var cart3 = [
   {"oranges": 91}
 ]
 for (var x = 0; x < cart3.length; x++){
-  var cartKey = Object.keys(cart3[x]))
+  var cartKey = Object.keys(cart3[x])
   console.log(cart3[x].cartKey)
   /*for (var prop in cart3[x]){
     console.log(cart3[x])
