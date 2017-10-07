2e0c5dbf3f92592b1f24becadef7007900388ac7
hmelo1 <heriberto@melo.nyc>
2017-10-07 20:06:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -90,15 +90,15 @@ for (var x = 0; x < cart3.length; x++){
     console.log(cart3[x])
   }*/
 }
-if (!itemsInCart.length){
+if (!itemsInCart2.length){
   console.log("Your shopping cart is empty.")
 }
-if (itemsInCart.length == 1){
-  console.log(`In your cart, you have ${itemsInCart[0]}.`)
+if (itemsInCart2.length == 1){
+  console.log(`In your cart, you have ${itemsInCart2[0]}.`)
 }
-if (itemsInCart.length == 2){
-  console.log(`In your cart, you have ${itemsInCart[0]} and ${itemsInCart[1]}.`)
+if (itemsInCart2.length == 2){
+  console.log(`In your cart, you have ${itemsInCart2[0]} and ${itemsInCart2[1]}.`)
 }
-if (itemsInCart.length >= 3){
-  console.log(`In your cart, you have ${itemsInCart[0]}, ${itemsInCart[1]}, and ${itemsInCart[2]}.`)
+if (itemsInCart2.length >= 3){
+  console.log(`In your cart, you have ${itemsInCart2[0]}, ${itemsInCart2[1]}, and ${itemsInCart2[2]}.`)
 }
