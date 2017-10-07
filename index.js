ed4ad359a235b0b64b081277392d70cb4f3f7b89
hmelo1 <heriberto@melo.nyc>
2017-10-07 17:50:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  // write your code here
+  var cartItems = {[item]: Math.floor(Math.random()*100)}
+  console.log(`${item} has been added to your cart.`)
+  cart.push(cartItems)
+  return cart
 }
 
 function viewCart() {
   // write your code here
+
+  if (!cart.length){
+    console.log("Your shopping cart is empty.")
+  }
+  if (cart2.length === 1){
+    console.log(`In your cart, you have ${cart2}`)
+  }
 }
 
 function total() {
@@ -27,4 +38,17 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+}
+var cart2 = {lemons: 64, mangos: 95, bananas: 32}
+
+for (var prop in cart2){
+  if (Object.keys(cart2).length == 1){
+    console.log(`In your cart, you have ${prop} at ${cart2[prop]}`)
+  }
+  if (Object.keys(cart2).length == 2 ){
+    console.log(`In your cart, you have ${prop} at ${cart2[prop]}`)
+  }
+  if (Object.keys(cart2).length >= 3){
+    console.log(`In your cart, you have ${prop} at ${cart2[prop]}`)
+  }
 }
