de8d3288d947f6cb86eea31a6cd750a06968580f
hmelo1 <heriberto@melo.nyc>
2017-10-07 18:30:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ var cart2 = {lemons: 64, mangos: 95, bananas: 32}
 var itemsInCart = []
 //for (let i = 0; i < Object.keys(cart2).length; i++){
   //itemsInCart.push(cart2)
-for (var prop in cart2){
+var prop in cart2
   itemsInCart.push(`${prop} at $${cart2[prop]}`)
   /*if (Object.keys(cart2).length == 1){
     console.log(`In your cart, you have ${itemsInCart[0]}.`)
@@ -64,4 +64,3 @@ for (var prop in cart2){
     console.log(`In your cart, you have ${prop} at ${cart2[prop]}`)
   }
   //move the if else out of the for. FOR should move items into array and if statements print of their own with array
-}
