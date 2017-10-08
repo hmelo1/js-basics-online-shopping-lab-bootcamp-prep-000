01aaafcc0df3b57aca94250918e892df9681b59e
hmelo1 <heriberto@melo.nyc>
2017-10-08 15:24:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ function placeOrder(cardNumber) {
     cart = []
 }
 
-cart3 = cart3.slice(x+1)
+
 var cart3 = [
   {"bananas": 64},
   {"mangos": 72},
@@ -86,9 +86,10 @@ var item = "mangos"
 for (var x = 0; x < cart3.length; x++){
   //var cartKey = Object.keys(cart3[x])
   if (!cart3[x].hasOwnProperty(item)){
-    console.log(`That item is not in your cart.`)
+    console.log("That item is not in your cart.")
   }
   else{
+  
     console.log(cart3)
   }
 }
