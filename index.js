8fa6c5eb7d35fecfecdb27812dfb2e8ab076e2fe
hmelo1 <heriberto@melo.nyc>
2017-10-08 03:58:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -66,6 +66,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  var cartTotal = total()
   if (!cardNumber){
     console.log("Sorry, we don't have a credit card on file for you.")
   }
