4b58d028545d7d7bf54ff413994fca42ab00f99c
hmelo1 <heriberto@melo.nyc>
2017-10-08 02:49:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ function removeFromCart(item) {
   // write your code here
   for (var x = 0; x < cart.length; x++){
     var cartKey = Object.keys(cart[x])
-    if (cart3[x].hasOwnProperty("bananas1")){
+    if (cart[x].hasOwnProperty(item)){
       console.log("Yay")
     }
     else{
