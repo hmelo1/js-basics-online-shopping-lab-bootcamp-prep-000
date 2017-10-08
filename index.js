3ec773bee635fe695d8cdef2749eafe55075789c
hmelo1 <heriberto@melo.nyc>
2017-10-08 15:19:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ var cart3 = [
   {"oranges": 91},
   {"apples": 82}
 ]
-var item = "bananas"
+var item = "mangos"
 for (var x = 0; x < cart3.length; x++){
   //var cartKey = Object.keys(cart3[x])
   if (!cart3[x].hasOwnProperty(item)){
